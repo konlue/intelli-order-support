6272ccf131cd79fabce654b4d2f6a308562c3fc1
konlue <2362586639@qq.com>
2026-04-11 20:53:58 +0800

    查看购物车

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -1,14 +1,14 @@
 package com.sky.controller.user;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user/shoppingCart")
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShoppingCartController {
     @Autowired
     private ShoppingCartService shoppingCartService;
+
     /**
      * 添加购物车
      */
@@ -24,5 +25,15 @@ public class ShoppingCartController {
         log.info("添加购物车：{}", shoppingCartDTO);
         shoppingCartService.addShoppingCart(shoppingCartDTO);
         return Result.success();
+    }
+
+    /**
+     * 查看购物车
+     */
+    @GetMapping("list")
+    public Result<List<ShoppingCart>> list(){
+        log.info("查看购物车");
+        List<ShoppingCart> list = shoppingCartService.showShoppingCart();
+        return Result.success(list);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -24,6 +24,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     private DishMapper dishMapper;
     @Autowired
     private SetmealMapper setmealMapper;
+
     /**
      * 添加购物车
      */
@@ -31,35 +32,49 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
         ShoppingCart shoppingCart = new ShoppingCart();
         BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
-        //用户只能查询自己的购物车
+        //只能查询自己的购物车数据
         shoppingCart.setUserId(BaseContext.getCurrentId());
-        //判断购物车是否已存在该菜品或套餐
+
+        //判断当前商品是否在购物车中
         List<ShoppingCart> shoppingCartList = shoppingCartMapper.list(shoppingCart);
+
         if (shoppingCartList != null && shoppingCartList.size() == 1) {
-            //如果存在，则数量加1
+            //如果已经存在，就更新数量，数量加1
             shoppingCart = shoppingCartList.get(0);
             shoppingCart.setNumber(shoppingCart.getNumber() + 1);
             shoppingCartMapper.updateNumberById(shoppingCart);
         } else {
-            //如果不存在，则插入数据，数量为1
+            //如果不存在，插入数据，数量就是1
+
             //判断当前添加到购物车的是菜品还是套餐
             Long dishId = shoppingCartDTO.getDishId();
             if (dishId != null) {
-                //判断为菜品
+                //添加到购物车的是菜品
                 Dish dish = dishMapper.getById(dishId);
                 shoppingCart.setName(dish.getName());
                 shoppingCart.setImage(dish.getImage());
                 shoppingCart.setAmount(dish.getPrice());
             } else {
-                //判断为套餐
+                //添加到购物车的是套餐
                 Setmeal setmeal = setmealMapper.getById(shoppingCartDTO.getSetmealId());
                 shoppingCart.setName(setmeal.getName());
                 shoppingCart.setImage(setmeal.getImage());
                 shoppingCart.setAmount(setmeal.getPrice());
             }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
         }
-        shoppingCart.setNumber(1);
-        shoppingCart.setCreateTime(LocalDateTime.now());
-        shoppingCartMapper.insert(shoppingCart);
+    }
+
+    /**
+     * 查看购物车
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        return shoppingCartMapper.list(ShoppingCart.
+                builder().
+                userId(BaseContext.getCurrentId()).
+                build());
     }
 }
