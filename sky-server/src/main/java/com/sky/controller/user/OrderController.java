f202dc8c010935812ac126acb19438edbb63eed7
konlue <2362586639@qq.com>
2026-04-12 11:10:28 +0800

    用户下单

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,31 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Slf4j
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 用户下单
+     */
+    @PostMapping("/submit")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO OrdersSubmitDTO){
+        log.info("用户下单：{}", OrdersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(OrdersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+}
